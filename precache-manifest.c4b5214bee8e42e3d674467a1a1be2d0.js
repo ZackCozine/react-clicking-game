d5f465e787ac9dcf7253b4247ba22697ab1e3a85
Zack C <zackcozine@yahoo.com>
2018-11-24 15:12:00 -0600

    Updates

---
+++ precache-manifest.c4b5214bee8e42e3d674467a1a1be2d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e9ebe015d080b451300",
+    "url": "/react-clicking-game/static/js/runtime~main.7e9ebe01.js"
+  },
+  {
+    "revision": "ea0cb53293a5c0689e81",
+    "url": "/react-clicking-game/static/js/main.ea0cb532.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/react-clicking-game/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "ea0cb53293a5c0689e81",
+    "url": "/react-clicking-game/static/css/main.a38bd581.chunk.css"
+  },
+  {
+    "revision": "ef6ddfb441892c26fd4039c5cffc380b",
+    "url": "/react-clicking-game/index.html"
+  }
+];
